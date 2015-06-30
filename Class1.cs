53a474ccbecb02fb4a18f2206ec18c88d00a36eb
Victor Grigoriu <vgrigoriu@gmail.com>
2015-06-30 20:11:39 +0300

    First version

---
+++ Class1.cs
@@ -0,0 +1,34 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LearnLinq
+{
+    public class ClassTests
+    {
+        public static int Sum(IEnumerable<int> ints)
+        {
+            return ints.Sum();
+        }
+        public static int Sum2(IEnumerable<int> ints)
+        {
+            var sum = 0;
+            foreach (var x in ints)
+            {
+                sum += x;
+            }
+
+            return sum;
+        }
+        public void CucuTest(Func<IEnumerable<int>, int> sum)
+        {
+            sum(new[] { 1, 2, 3 }).ShouldBe(6);
+        }
+
+        //public void IntTest(int x)
+        //{
+        //    x.ShouldBe(2);
+        //}
+    }
+}
---
+++ LambdaConvention.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Fixie;
+using System.Linq;
+
+namespace LearnLinq
+{
+    public class LambdaConvention: Convention
+    {
+        public LambdaConvention()
+        {
+            Classes.Where(@class => @class.IsVisible);
+            Parameters.Add<FromMarkedFunctions>();
+        }
+    }
+
+    internal class FromMarkedFunctions : ParameterSource
+    {
+        public IEnumerable<object[]> GetParameters(MethodInfo method)
+        {
+            if (typeof(Delegate).IsAssignableFrom(method.GetParameters().First().ParameterType))
+            {
+                var testType = method.DeclaringType.GetTypeInfo();
+
+                var ms = testType.DeclaredMethods;
+                foreach (var m in ms)
+                {
+                    var del = Delegate.CreateDelegate(method.GetParameters().First().ParameterType, m, false);
+                    Console.WriteLine(del);
+                }
+
+                var methods = from m in testType.DeclaredMethods
+                              let del = Delegate.CreateDelegate(method.GetParameters().First().ParameterType, m, false)
+                              where del != null
+                              select new object[] { del };
+
+                foreach (var del in methods)
+                {
+                    yield return del;
+                }
+            }
+            else
+            {
+                yield return new object[] { 2 };
+            }
+        }
+    }
+}
